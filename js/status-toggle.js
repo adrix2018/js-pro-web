4faf7e62b6c771831c6faf1a87b7f28b993dbd76
Eduardo Lemos <eddu1930@gmail.com>
2018-01-24 17:29:54 -0500

    Add files via upload

---
+++ js/status-toggle.js
@@ -0,0 +1,17 @@
+$( "#button1" ).click(function() {
+$( "#status-red" ).show();
+$( "#status-blue" ).hide();
+$( "#status-yellow" ).hide();
+});
+
+$( "#button2" ).click(function() {
+$( "#status-red" ).hide();
+$( "#status-blue" ).show();
+$( "#status-yellow" ).hide();
+});
+
+$( "#button3" ).click(function() {
+$( "#status-red" ).hide();
+$( "#status-blue" ).hide();
+$( "#status-yellow" ).show();
+});
